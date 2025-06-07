0c0aa59f4049529cfc4fe3e7623168930584477d
prik73 <prinovac@gmail.com>
2025-06-07 09:57:39 +0530

    updated vite config to make use of cert and key for https

--- react-setup/frontend/vite.config.js
+++ react-setup/frontend/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
     port: 5173,
-    https: true,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../backend/ssl/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../backend/ssl/cert.pem'))
+    },
     proxy: {
       '/socket.io': {
         target: 'https://localhost:3000',
